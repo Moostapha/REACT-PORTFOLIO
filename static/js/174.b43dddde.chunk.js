9707e039e9afd1f0d6120944e953838a0d58e6f6
Moustapha Touré <moostoure@gmail.com>
2023-05-26 18:05:33 +0200

    Updates

---
+++ static/js/174.b43dddde.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkporfolio=self.webpackChunkporfolio||[]).push([[174],{174:function(o,e,f){f.r(e),e.default={}}}]);
+//# sourceMappingURL=174.b43dddde.chunk.js.map
---
+++ static/js/387.aa7fd2b9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkporfolio=self.webpackChunkporfolio||[]).push([[387],{387:function(o,e,f){f.r(e),e.default={}}}]);
+//# sourceMappingURL=387.aa7fd2b9.chunk.js.map
